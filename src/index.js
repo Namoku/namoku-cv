016fc848f7cc95914fee1bf44368020ee7fc69aa
Namoku <31052826+Namoku@users.noreply.github.com>
2021-08-17 00:28:39 -0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
-import { StrictMode } from "react";
+import { StrictMode, useState } from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
+function Boton() {
+  const [count, setCount] = useState(0);
+  return (
+    <div className="Boton">
+      <button onClick={() => setCount(count + 1)}>Have fun!</button>
+      <p>You clicked {count} times</p>
+    </div>
+  );
+}
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+    </div>
+  );
+}
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
     <App />
+    <Boton />
   </StrictMode>,
   rootElement
 );
