75368efbf6423e7c1f93d39a1a3903a28471ae25
Namoku <31052826+Namoku@users.noreply.github.com>
2021-08-16 21:33:51 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Namoku - CV</h1>
+      <h2>Under construction!</h2>
+    </div>
+  );
+}
